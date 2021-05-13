259e18746e2c286531afda0ec52e73f4f67f91c4
Jason F. Irwin <jason@matigo.ca>
2021-05-13 23:13:41 +0900

    21E130
    Update Includes:
    * correct versions.php file for the stage

--- conf/versions.php
+++ conf/versions.php
@@ -4,7 +4,7 @@
  * @author Jason F. Irwin
  */
 
-define('APP_VER', '21E120');                    // The Application Version
+define('APP_VER', '21E130');                    // The Application Version
 define('GENERATOR', 'Kiroku v0.1a');            // Generator Name
 
 ?>
