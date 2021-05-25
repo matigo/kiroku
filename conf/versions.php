358018e6131ef7a48f0155b22fb1ed170774e56a
Jason F. Irwin <jason@matigo.ca>
2021-05-25 17:24:44 +0900

    21E250
    Update Includes:
    * a little more effort on the Author theme

--- conf/versions.php
+++ conf/versions.php
@@ -4,7 +4,7 @@
  * @author Jason F. Irwin
  */
 
-define('APP_VER', '21E240');                    // The Application Version
+define('APP_VER', '21E250');                    // The Application Version
 define('GENERATOR', 'Kiroku v0.1a');            // Generator Name
 
 ?>
